a0eff1a1f206a07a973cc34c26b67825939ee8cb
Daryl <daryl.sinon@tkservices.co>
2022-05-13 13:43:55 +0800

    add: Testing response

--- conversion/handler.js
+++ conversion/handler.js
@@ -2,9 +2,8 @@
 
 module.exports = async (event, context) => {
   const result = {
-    body: JSON.stringify(event.body),
+    body: JSON.stringify({ name: 'Daryl', position: 'Fullstack Developer' }),
     'content-type': event.headers['content-type'],
-    'conversion-api': 'Success version 1',
   };
 
   return context.status(200).succeed(result);
