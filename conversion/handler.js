984dfbcc3e567d83ed526901152da06ecdb5b9b9
Daryl <daryl.sinon@tkservices.co>
2022-05-13 14:11:24 +0800

    update: Change response

--- conversion/handler.js
+++ conversion/handler.js
@@ -1,9 +1,13 @@
 'use strict';
 
 module.exports = async (event, context) => {
+  // const result = {
+  //   body: JSON.stringify({ name: 'Daryl', position: 'Fullstack Developer' }),
+  //   'content-type': event.headers['content-type'],
+  // };
+
   const result = {
-    body: JSON.stringify({ name: 'Daryl', position: 'Fullstack Developer' }),
-    'content-type': event.headers['content-type'],
+    status: 'Received input: ' + JSON.stringify(event.body),
   };
 
   return context.status(200).succeed(result);
