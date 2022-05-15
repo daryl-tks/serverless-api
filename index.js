1fb5aa707b529f7326d5f06dcfcd0a6c5f0788a3
Daryl <daryl.sinon@tkservices.co>
2022-05-15 20:28:24 +0800

    change folder name to conversion

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const handler = require('./routes/handler');
+const handler = require('./conversion/handler');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
