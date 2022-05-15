234b88aa00a15f298f7af70383d7c759196133ed
Daryl <daryl.sinon@tkservices.co>
2022-05-15 19:48:20 +0800

    fix: Codes for deployment

--- routes/handler.js
+++ routes/handler.js
@@ -2,24 +2,22 @@
 
 const { getUsers } = require('../service/users/get-users');
 
-// module.exports = async (event, context) => {
-//   console.log('CONTEXT', context);
+module.exports = async (event, context) => {
+  const result = {
+    body: { name: 'Daryl', age: 29 },
+    'content-type': 'application/json',
+  };
 
-//   const result = {
-//     body: { name: 'Daryl', age: 29 },
-//     'content-type': 'application/json',
-//   };
-
-//   return context.status(200).succeed(result);
-// };
-
-module.exports = (event, context) => {
-  if (event.path == '/users') {
-    return getUsers(event, context);
-  }
-
-  return context.status(200).succeed('Welcome to conversion API');
+  return context.status(200).succeed(result);
 };
+
+// module.exports = (event, context) => {
+//   if (event.path == '/users') {
+//     return getUsers(event, context);
+//   }
+
+// return context.status(200).succeed('Welcome to conversion API');
+// };
 
 // function users(event, context) {
 //   return context.status(200).succeed(['Jean', 'Joe', 'jane']);
