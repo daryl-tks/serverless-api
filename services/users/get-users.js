383bb069ee8cbbfea789b63f476d68c81a9ac255
Daryl <daryl.sinon@tkservices.co>
2022-05-16 13:50:17 +0800

    restructure files

--- services/users/get-users.js
+++ services/users/get-users.js
@@ -1,23 +1,40 @@
-const res = require('express/lib/response');
+// const res = require('express/lib/response');
 const connection = require('../../utils/connection');
 
-const getUsers = async (event, context) => {
-  try {
-    const result = await connection
-      .promise()
-      .query('SELECT * FROM users')
-      .then(([rows, fields]) => {
-        return rows;
-      })
-      .catch((err) => console.error({ err }));
+// const getUsers = async (event, context) => {
+//   try {
+//     const result = await connection
+//       .promise()
+//       .query('SELECT * FROM users')
+//       .then(([rows, fields]) => {
+//         return rows;
+//       })
+//       .catch((err) => console.error({ err }));
 
-    return context
-      .headers({ 'Content-Type': 'Application/Json' })
-      .status(200)
-      .succeed({ data: result });
-  } catch (error) {
-    throw console.error({ get_users_error: error });
-  }
-};
+//     return context
+//       .headers({ 'Content-Type': 'Application/Json' })
+//       .status(200)
+//       .succeed({ data: result });
+//   } catch (error) {
+//     throw console.error({ get_users_error: error });
+//   }
+// };
 
-module.exports = { getUsers };
+// module.exports = { getUsers };
+
+var express = require('express');
+var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function (req, res, next) {
+//   res.send({ name: 'USERS' });
+// });
+
+// @DESC - GET users
+router.get('/', function (_, res) {
+  connection.query('SELECT * FROM users', (err, result) => {
+    !err ? res.send({ data: result }) : res.status(400).send({ err_msg: err });
+  });
+});
+
+module.exports = router;
