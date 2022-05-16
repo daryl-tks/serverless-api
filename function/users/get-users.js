8ec691f4f44ef9b833fce9262d4b01751424ee38
Daryl <daryl.sinon@tkservices.co>
2022-05-16 10:59:27 +0800

    add: response

--- function/users/get-users.js
+++ function/users/get-users.js
@@ -3,15 +3,25 @@ const connection = require('../connection');
 
 const getUsers = async (_event, context) => {
   try {
-    const result = await connection
-      .promise()
-      .query('SELECT * FROM users')
-      .then(([rows, _fields]) => rows);
+    // const result = await connection
+    //   .promise()
+    //   .query('SELECT * FROM users')
+    //   .then(([rows, _fields]) => rows);
 
-    return context
+    return await context
       .headers({ 'Content-Type': 'Application/Json' })
       .status(200)
-      .succeed(JSON.stringify({ data: result }));
+      .succeed(
+        JSON.stringify({
+          data: [
+            {
+              user_id: 1,
+              username: 'hello',
+              created_at: '2022-05-15T17:29:04.000Z',
+            },
+          ],
+        })
+      );
   } catch (error) {
     throw console.error({ get_users_error: error });
   }
