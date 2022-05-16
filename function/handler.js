5f69d33e68a9546528e9df5f4b1b0a632bfe4f34
Daryl <daryl.sinon@tkservices.co>
2022-05-16 10:22:01 +0800

    fix: DB Connection

--- function/handler.js
+++ function/handler.js
@@ -1,7 +1,7 @@
 'use strict';
 // const connection = require('./connection');
-var mysql = require('mysql2');
 require('dotenv').config();
+var mysql = require('mysql2');
 
 var connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -19,27 +19,31 @@ module.exports = async (event, context) => {
   return context.status(200).succeed('Welcome to conversion API');
 };
 
-const getUsers = async (_event, context) => {
-  try {
-    const result = await connection
-      .promise()
-      .query('SELECT * FROM users')
-      .then(([rows, _fields]) => rows)
-      .catch((err) => console.error({ err }));
+const getUsers = (_event, context) => {
+  // try {
+  // const result = await connection
+  //   .promise()
+  //   .query('SELECT * FROM users')
+  //   .then(([rows, _fields]) => rows)
+  //   .catch((err) => console.error({ err }));
 
-    return context
-      .headers({ 'Content-Type': 'Application/Json' })
-      .status(200)
-      .succeed({
-        data: [
-          {
-            user_id: 1,
-            username: 'hello',
-            created_at: '2022-05-15T17:29:04.000Z',
-          },
-        ],
-      });
-  } catch (error) {
-    throw console.error({ get_users_error: error });
-  }
+  connection.connect((err) => {
+    !!err ? console.log('ERROR', err) : console.log('DB SERVER CONNECTED');
+  });
+
+  return context
+    .headers({ 'Content-Type': 'Application/Json' })
+    .status(200)
+    .succeed({
+      data: [
+        {
+          user_id: 1,
+          username: 'hello',
+          created_at: '2022-05-15T17:29:04.000Z',
+        },
+      ],
+    });
+  // } catch (error) {
+  //   throw console.error({ get_users_error: error });
+  // }
 };
