2ed139c8d26680c4816ff22102e4e0014d243c27
Daryl <daryl.sinon@tkservices.co>
2022-05-16 10:50:42 +0800

    revert

--- function/users/get-users.js
+++ function/users/get-users.js
@@ -1,21 +1,17 @@
+const res = require('express/lib/response');
 const connection = require('../connection');
 
 const getUsers = async (_event, context) => {
   try {
-    return await connection
+    const result = await connection
       .promise()
       .query('SELECT * FROM users')
-      .then(([rows, _fields]) =>
-        context
-          .headers({ 'Content-Type': 'Application/Json' })
-          .status(200)
-          .succeed(JSON.stringify({ data: rows }))
-      );
+      .then(([rows, _fields]) => rows);
 
-    // return context
-    //   .headers({ 'Content-Type': 'Application/Json' })
-    //   .status(200)
-    //   .succeed(JSON.stringify({ data: result }));
+    return context
+      .headers({ 'Content-Type': 'Application/Json' })
+      .status(200)
+      .succeed(JSON.stringify({ data: result }));
   } catch (error) {
     throw console.error({ get_users_error: error });
   }
