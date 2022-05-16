5145b13a4205d423e89735a33b88117d7bd70b8d
Daryl <daryl.sinon@tkservices.co>
2022-05-16 09:23:43 +0800

    add: Routing

--- function/handler.js
+++ function/handler.js
@@ -3,18 +3,26 @@
 // const { getUsers } = require('../services/users/get-users');
 
 // module.exports = async (event, context) => {
+//   if (event.path == '/users') {
+//     return context.status(200).succeed(['Jean', 'Joe', 'jane']);
+//   }
+
 //   const result = {
-//     body: JSON.stringify(event.body),
+//     body: 'Welcome to conversion API',
 //     'content-type': event.headers['content-type'],
 //   };
 
 //   return context.status(200).succeed(result);
 // };
 
+function getUsers(event, context) {
+  return context.status(200).succeed(['Jean', 'Joe', 'jane']);
+}
+
 module.exports = (event, context) => {
-  // if (event.path == '/users') {
-  //   return getUsers(event, context);
-  // }
+  if (event.path == '/users') {
+    return getUsers(event, context);
+  }
 
   return context.status(200).succeed('Welcome to conversion API');
 };
