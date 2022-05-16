3082afa077622db38917d11462d1a96ca66e0e38
Daryl <daryl.sinon@tkservices.co>
2022-05-16 10:44:53 +0800

    fix: DB

--- function/users/get-users.js
+++ function/users/get-users.js
@@ -1,18 +1,21 @@
-const res = require('express/lib/response');
 const connection = require('../connection');
 
 const getUsers = async (_event, context) => {
   try {
-    const result = await connection
+    return await connection
       .promise()
       .query('SELECT * FROM users')
-      .then(([rows, _fields]) => rows)
-      .catch((err) => console.error({ err }));
+      .then(([rows, _fields]) =>
+        context
+          .headers({ 'Content-Type': 'Application/Json' })
+          .status(200)
+          .succeed(JSON.stringify({ data: rows }))
+      );
 
-    return context
-      .headers({ 'Content-Type': 'Application/Json' })
-      .status(200)
-      .succeed(JSON.stringify({ data: result }));
+    // return context
+    //   .headers({ 'Content-Type': 'Application/Json' })
+    //   .status(200)
+    //   .succeed(JSON.stringify({ data: result }));
   } catch (error) {
     throw console.error({ get_users_error: error });
   }
