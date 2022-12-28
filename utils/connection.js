d40ea2008ccb396d89cd2638517b892e37ffea7e
Daryl <daryl.sinon@tkservices.co>
2022-12-28 16:29:13 +0800

    remove creds

--- utils/connection.js
+++ utils/connection.js
@@ -3,8 +3,8 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'daryl',
-  password: 'D@rylSinon1993',
-  database: 'khelraja',
+  password: '',
+  database: '',
 });
 
 // connection.connect((err) => {
